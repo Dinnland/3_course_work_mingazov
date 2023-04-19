ef7a3c98a161a3258a7642c8e31509f73a6084e0
Dinar Mingazov <dinarek2013@gmail.com>
2023-04-19 04:27:06 +0300

    i was finished tests with pytest 88% 3

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,6 @@
 from utils import utils
 
 
-# from utils.utils import sort_data
-
-
 def test_sort_data(test_data):
     sorted_data = utils.sort_data(test_data)
     assert [x['date'] for x in sorted_data] == ['2019-08-26T10:50:58.294041', '2019-07-03T18:35:29.512364',
